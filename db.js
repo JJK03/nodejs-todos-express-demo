49a8c9d6a44c1a59ea5debf8796f0398f39b4a27
JJK03 <wkdwlsrb09@gmail.com>
2025-10-31 14:29:46 +0900

    nopw

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "Wkdwlsrb1!",
+    password: "rootroot",
     database: "my7287db",
     waitForConnections: true,
     connectionLimit: 10,
